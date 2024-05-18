0a286013a0904c5a3b4fd6742f6e46ca5999555b
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-18 18:05:00 +0200

    Show all product

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,22 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import { simpleProducts } from './data'
 
 const App = () => {
   return (
     <div>
       <header>TS EmarhZone</header>
-      <main></main>
+      <main>
+        <ul>
+          {simpleProducts.map((product) => (
+            <li>
+              <img src={product.image} alt="" />
+              <h2>{product.name}</h2>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All Right Reserved</footer>
     </div>
   )
